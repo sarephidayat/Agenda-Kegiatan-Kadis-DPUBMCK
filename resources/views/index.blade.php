fa925355e3d35b3346b9b048ff14dccee14b9571
unknown <fajarirhamh18@gmail.com>
2025-07-01 09:17:52 +0700

    asu index ki

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,28 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Agenda Kegiatan Kadis</title>
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <div style="">
-        <div style="border: 1px solid black; padding: 10px; margin: 10px;">
-            <h1>Halaman Kadis</h1>
-            <a href="kadis">Halaman Kadis</a>
-        </div>
-        <div style="border: 1px solid black; padding: 10px; margin: 10px;">
-            <h1>Halaman Sub Koor</h1>
-            <a href="sub-koor">Halaman Sub.Koordinator</a>
-        </div>
-        <div style="border: 1px solid black; padding: 10px; margin: 10px;">
-            <h1>Halaman Staff</h1>
-            <a href="staff">Halaman Staff</a>
-        </div>
-        <div style="border: 1px solid black; padding: 10px; margin: 10px;">
-            <h1>Halaman TU</h1>
-            <a href="tu">Halaman TU</a>
+<body class="bg-light">
+    <div class="container py-5">
+        <h1 class="mb-4 text-center fw-bold text-primary">Agenda Kegiatan Kadis</h1>
+        <div class="row g-4 justify-content-center">
+            <div class="col-md-5 col-lg-4">
+                <div class="card shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Halaman Kadis</h5>
+                        <p class="card-text">Akses halaman khusus Kadis untuk melihat dan mengelola agenda.</p>
+                        <a href="kadis" class="btn btn-primary">Halaman Kadis</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <div class="card shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Halaman Sub Koor</h5>
+                        <p class="card-text">Akses halaman Sub Koordinator untuk agenda dan tugas terkait.</p>
+                        <a href="sub-koor" class="btn btn-success">Halaman Sub.Koordinator</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <div class="card shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Halaman Staff</h5>
+                        <p class="card-text">Halaman khusus Staff untuk melihat agenda dan aktivitas.</p>
+                        <a href="staff" class="btn btn-warning">Halaman Staff</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <div class="card shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Halaman TU</h5>
+                        <p class="card-text">Akses halaman Tata Usaha untuk pengelolaan administrasi.</p>
+                        <a href="tu" class="btn btn-info text-white">Halaman TU</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    
+    <!-- Bootstrap JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
